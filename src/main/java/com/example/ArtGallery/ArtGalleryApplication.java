487d3539cbe528b44868ed1f31174a69bb045004
KaterinaS. <153716458+KaterinaSelem@users.noreply.github.com>
2024-07-29 09:16:14 +0200

    new

--- src/main/java/com/example/ArtGallery/ArtGalleryApplication.java
+++ src/main/java/com/example/ArtGallery/ArtGalleryApplication.java
@@ -3,6 +3,13 @@ package com.example.ArtGallery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+
+
 @SpringBootApplication
 public class ArtGalleryApplication {
 
@@ -10,4 +17,17 @@ public class ArtGalleryApplication {
 		SpringApplication.run(ArtGalleryApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**")
+						.allowedOrigins("http://localhost:5173")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
+	}
 }
